14be99d7fb37f19f52eba696482fbe5c6f7c69c7
Imag0mortis <117734513+Imag0mortis@users.noreply.github.com>
2023-08-24 12:03:13 +0300

    посмотреть

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,30 +21,6 @@ export class AppComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.getData();
     this.appService.init();
-    this.loadUsetifulScript(this.userId);
-  }
-
-  private loadUsetifulScript(userId: string): void {
-    const usetifulTags = { userId };
-    
-
-    const script = document.createElement('script');
-    script.async = true;
-    script.src = 'https://www.usetiful.com/dist/usetiful.js';
-    script.setAttribute('id', 'usetifulScript');
-    script.dataset['token'] = 'ff8f5b44ca52aed9b607ddfd7484bcac';
-
-    script.textContent = `window.usetifulTags = ${JSON.stringify(usetifulTags)};`;
-
-    const head = document.getElementsByTagName('head')[0];
-    head.appendChild(script);
-  }
-
-  getData() {
-    this.requestService.getUserInfo().subscribe((r: any) => {
-      this.userId = r.taskList;
-    });
   }
 }
--- src/app/selfransom/selfransom.component.ts
+++ src/app/selfransom/selfransom.component.ts
@@ -1,15 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  Inject,
+  OnInit,
+  Renderer2
+} from '@angular/core';
 import { SelfransomService } from '../shared/services/selfransom.service';
+import { RequestService } from '../shared/services/request.service';
+import { AppService } from '../shared/services/app.service';
 
 @Component({
   selector: 'app-selfransom',
   templateUrl: './selfransom.component.html',
-  styleUrls: ['./selfransom.component.scss']
+  styleUrls: ['./selfransom.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SelfransomComponent implements OnInit {
-  constructor(private selfransomService: SelfransomService) {}
+  
+  userId: string;
 
-  ngOnInit(): void {
+  constructor(private selfransomService: SelfransomService,
+    private appService: AppService,
+    private requestService: RequestService,
+    private ref: ChangeDetectorRef,
+    ) {}
+
+  async ngOnInit(): Promise <void> {
     this.selfransomService.getUserGeoLocation();
+    this.appService.init();
+    await this.getData();
+    await this.loadUsetifulScript(this.userId);
+    setTimeout(() => {
+      console.log('Айдишник', this.userId)
+    }, 5000);
+    this.ref.detectChanges();
+  }
+
+  private loadUsetifulScript(userId: string): void {
+    const usetifulTags = { userId };
+    
+
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://www.usetiful.com/dist/usetiful.js';
+    script.setAttribute('id', 'usetifulScript');
+    script.dataset['token'] = 'ff8f5b44ca52aed9b607ddfd7484bcac';
+
+    script.textContent = `window.usetifulTags = ${JSON.stringify(usetifulTags)};`;
+
+    const head = document.getElementsByTagName('head')[0];
+    head.appendChild(script);
+  }
+
+  getData() {
+    this.requestService.getUserInfo().subscribe((r: any) => {
+      this.userId = r.userId;
+    });
   }
 }
